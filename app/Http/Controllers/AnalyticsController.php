bf400db09a122733ff8aefc32f77063c864f09b9
s1nju <78903844+amine502@users.noreply.github.com>
2025-11-30 19:51:57 +0100

    add:analytics controller

---
+++ app/Http/Controllers/AnalyticsController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DETAIL_EMPRUNT;
+use App\Models\EMPRUNT;
+use Illuminate\Http\Request;
+use App\Models\LIVRE;
+use Illuminate\Support\Facades\DB;
+
+class AnalyticsController extends Controller
+{
+    //
+    public function getLivresCount()
+    {
+        $count = LIVRE::count();
+        return response()->json(['count' => $count], 200);
+    }
+    public function getEmpruntsCount()
+    {
+        $count = EMPRUNT::count();
+        return response()->json(['count' => $count], 200);
+    }
+    public function getTopEmpruntedLivres()
+    {
+        $topLivres_IDS = DETAIL_EMPRUNT::select('livre_id', DB::raw('count(*) as total'))
+            ->groupBy('livre_id')
+            ->orderBy('total', 'desc')
+            ->with('livre')
+            ->take(10)
+            ->get();
+        $topLivres = [];
+        foreach ($topLivres_IDS as $item) {
+            $livre = LIVRE::find($item->livre_id);
+            if ($livre) {
+                $topLivres[] = [
+                    'livre' => $livre,
+                    'emprunt_count' => $item->total,
+                ];
+            }
+        }
+
+        return response()->json(['top_livres' => $topLivres], 200);
+    }
+    public function getFrequentEmprunteurs()
+    {
+        $topEtudiants = EMPRUNT::select('etudiant_id', DB::raw('count(*) as total'))
+            ->groupBy('etudiant_id')
+            ->orderBy('total', 'desc')
+            ->take(10)
+            ->get();
+
+        return response()->json(['top_etudiants' => $topEtudiants], 200);
+    }
+    public function getRetardsCount()
+    {
+        $count = DB::table('retards')->count();
+        return response()->json(['count' => $count], 200);
+    }
+    public function getRetardEmprenturs()
+    {
+        $topEtudiants = DB::table('emprunts')
+            ->join('retards', 'emprunts.id', '=', 'retards.emprunt_id')
+            ->select('emprunts.etudiant_id', DB::raw('count(*) as total'))
+            ->groupBy('emprunts.etudiant_id')
+            ->orderBy('total', 'desc')
+            ->take(10)
+            ->get();
+
+        return response()->json(['top_etudiants_with_retards' => $topEtudiants], 200);
+    }
+    
+}
